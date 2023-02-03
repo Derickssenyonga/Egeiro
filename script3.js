639c1815c5bb01ff545006330a1ce7633e3c83cf
Derickssenyonga <124336196+Derickssenyonga@users.noreply.github.com>
2023-02-03 13:47:56 +0300

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,7 @@
+﻿var menuIcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
+menuIcon.onclick=function(){
+sidebar.classList.toggle("small-sidebar");
+container.classList.toggle("large-container");
+}
